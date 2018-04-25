6ba9443dcb4839c1799df556bc11c6b9528a5b0e
HaydenTuckerQA <Hayden.Tucker@qa.com>
2018-04-25 17:25:32 +0100

    Fixing persistence

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO MOVIE (id, title, genre, rating) VALUES (1, 'Harry Potter', 'Fantasy', 'U')
+INSERT INTO MOVIE (id, title, genre, rating) VALUES (2, 'Star Wars Episode 1', 'Documentary', 'Unwatchable')

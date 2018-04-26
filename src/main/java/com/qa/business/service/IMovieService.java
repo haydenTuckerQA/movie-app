d7e522498192aacd8d81f1a24bc225cf4ae6da40
HaydenTuckerQA <Hayden.Tucker@qa.com>
2018-04-26 10:06:58 +0100

    Implemented getMovie and createMovie features

--- src/main/java/com/qa/business/service/IMovieService.java
+++ src/main/java/com/qa/business/service/IMovieService.java
@@ -2,4 +2,6 @@ package com.qa.business.service;
 
 public interface IMovieService {
 	String getAllMovies();
+	String getMovie(Long id);
+	String createMovie(String movieAsJSON);
 }
--- src/main/java/com/qa/interoperability/MovieEndpoint.java
+++ src/main/java/com/qa/interoperability/MovieEndpoint.java
@@ -1,8 +1,11 @@
 package com.qa.interoperability;
 
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 import com.qa.business.service.IMovieService;
@@ -18,5 +21,20 @@ public class MovieEndpoint {
 	@Produces({ "application/json" })
 	public String getAllMovies() {
 		return service.getAllMovies();
+	}
+	
+	@GET
+	@Path("/json/{id}")
+	@Produces({ "application/json" })
+	public String getMovie(@PathParam("id") Long id) {
+		return service.getMovie(id);
+	}
+	
+	@POST
+	@Path("/json")
+	@Produces({ "application/json" })
+	@Consumes({ "application/json" })
+	public String createMovie(String movieAsJSON) {
+		return service.createMovie(movieAsJSON);
 	}
 }
